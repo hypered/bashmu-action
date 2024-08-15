c322e0fc39b8cf9027f0ae88040ea07bef97db90
Vo Minh Thu <thu@hypered.io>
2024-08-15 23:43:49 +0200

    Log when entering the main function.

--- dist/index.js
+++ dist/index.js
@@ -29220,14 +29220,15 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(2186));
 const github = __importStar(__nccwpck_require__(5438));
 async function main() {
+    console.log('Main function');
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput('time', time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
+    const time = (new Date()).toTimeString();
+    core.setOutput('time', time);
 }
 async function post() {
     console.log('Post function');
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,18 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 async function main() {
+  console.log('Main function');
+
   // `who-to-greet` input defined in action metadata file
   const nameToGreet: string = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
 
-  const time: string = (new Date()).toTimeString();
-  core.setOutput('time', time);
-
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload: string = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+  const time: string = (new Date()).toTimeString();
+  core.setOutput('time', time);
 }
 
 async function post() {
